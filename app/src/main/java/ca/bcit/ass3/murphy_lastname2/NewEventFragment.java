ca924155bb9aca2f3bb673ef621195ebba95748e
William Murphy <will.murphy1994@gmail.com>
2017-11-10 12:42:38 -0800

    Item list now updates when an item is added

--- app/src/main/java/ca/bcit/ass3/murphy_lastname2/NewEventFragment.java
+++ app/src/main/java/ca/bcit/ass3/murphy_lastname2/NewEventFragment.java
@@ -38,7 +38,6 @@ public class NewEventFragment extends DialogFragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
